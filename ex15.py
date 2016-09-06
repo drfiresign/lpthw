1f31bae04ccf07fa376b1882bbc2db167d5aaa16
Dylan Cascio <Dcascio73@gmail.com>
2016-09-05 18:38:43 -0700

    ex15: removed the second file input

--- ex15.py
+++ ex15.py
@@ -11,13 +11,3 @@ txt = open(filename)
 # the contents of the file
 print "Here's your file %r:" % filename
 print txt.read()
-
-# receives the name of the file as input
-print "Type the filename again:"
-file_again = raw_input("> ")
-
-# creates another variable to stand as the open file
-txt_again = open(file_again)
-
-# prints the data contained inside the file
-print txt_again.read()
