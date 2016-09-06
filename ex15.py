5976da9e80173458b2d2e29111f1334d39fcabc4
Dylan Cascio <Dcascio73@gmail.com>
2016-09-05 18:37:46 -0700

    ex15: commented the script

--- ex15.py
+++ ex15.py
@@ -1,15 +1,23 @@
+# imports the argv method from the sys module
 from sys import argv
 
+# assigns the arguments passed in the CLi as usable arguments
 script, filename = argv
 
+# sets the file itself as a variable using the open() function
 txt = open(filename)
 
+# formats the file and uses the .read() function to actually read
+# the contents of the file
 print "Here's your file %r:" % filename
 print txt.read()
 
+# receives the name of the file as input
 print "Type the filename again:"
 file_again = raw_input("> ")
 
+# creates another variable to stand as the open file
 txt_again = open(file_again)
 
+# prints the data contained inside the file
 print txt_again.read()
