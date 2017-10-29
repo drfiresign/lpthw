0159ca5405fc8361e2765eea1a983ed2bae8e4fc
Dylan Cascio <Dcascio73@gmail.com>
2017-10-28 22:43:03 -0700

    ex17: shortened input read line, added if statment to break if to file doesn't exist.

--- ex17.py
+++ ex17.py
@@ -6,19 +6,20 @@ script, from_file, to_file = argv
 print(f"Copying from {from_file} to {to_file}")
 
 # we could do these on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
 
 print(f"The input file is {len(indata)} byes long")
 
-print(f"Does the output file exist? {exists(to_file)}")
-print("Ready, hit RETURN to continue, CTRL-C to abort.")
-input()
+if exists(to_file):
+    continue
+else:
+    print("To file not found.")
+    break
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
-print("Alright, all done.")
+print("All done. Bye bye.")
 
 out_file.close()
 in_file.close()
