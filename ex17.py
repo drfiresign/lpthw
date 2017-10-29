ddbcad0d5db2a4dd3936506204b530ec07114543
Dylan Cascio <Dcascio73@gmail.com>
2017-10-29 16:40:57 -0700

    I think that I fixed something last night.
    Clearly I forgot to actually summarize whatever the hell I did here.

--- ex17.py
+++ ex17.py
@@ -1,25 +1,4 @@
 from sys import argv
-from os.path import exists
 
 script, from_file, to_file = argv
-
-print(f"Copying from {from_file} to {to_file}")
-
-# we could do these on one line, how?
-indata = open(from_file).read()
-
-print(f"The input file is {len(indata)} byes long")
-
-if exists(to_file):
-    continue
-else:
-    print("To file not found.")
-    break
-
-out_file = open(to_file, 'w')
-out_file.write(indata)
-
-print("All done. Bye bye.")
-
-out_file.close()
-in_file.close()
+out_file = open(to_file, 'w').write(open(from_file).read())
