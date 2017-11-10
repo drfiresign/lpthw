460db1cdd5a86d9b8bfa6c48702eadef77e6c1c0
Dylan Cascio <dcascio73@gmail.com>
2017-11-10 14:07:23 -0500

    Ex36: Creating my own game
    New exercise to create my own text adventure using as much of what has been
    covered as I can. I am trying to make a game with several options to choose from.

---
+++ ex36.py
@@ -0,0 +1,63 @@
+# begin in a field
+
+# see a river and an entrance to a cave
+
+# field function
+
+    # You can enter the cave
+        # cave function
+
+    # You can visit the river
+        # You are swept away
+
+# river function
+    # text about how nice the water is
+    # you hear a beautiful song of a siren
+    # the water is so inviting
+    # you step further into the water
+    # you begin to drift to sleep
+    # you are swept away and drown, blissfully
+
+# cave function
+    # you see an armory
+        # armory function
+
+    # you see a wine cellar
+        # wine cellar function
+
+    # you see a hall (of heroes)
+        # hall function
+
+# armory function
+    # you see many suits of armor
+    # you can search the room
+        # find a key
+        # key = True
+    # skip the room
+        # return to cave function
+
+# wine cellar function
+    # you see many casks
+        # give them names
+    # you can drink from a cask
+        # drunk = True
+        # spawn separate dialog options for drunk
+    # skip the room
+        # return to cave function
+
+# hall function
+    # if you have the key
+        # find the passage to... where?
+    # if you don't have the key
+        # you pull a level and the door closes sealing you in
+        # your torch begins to fade
+        # you starve to death in total darkness
+    # if you have the key and are drunk
+        # you stumble into a wall and break through into a treasure chamber
+    # if you don't have the key and are drunk
+        # you stumble onto a floor switch activating a trap door which impales you on spikes
+        # you feel no pain because of how drunk you are
+
+# passage function
+    # ???
+    # profit!
