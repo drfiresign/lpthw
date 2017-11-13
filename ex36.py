38e3f465eba561484cbfc5b35aac1b7b14dc1652
Dylan Cascio <dcascio73@gmail.com>
2017-11-12 19:29:42 -0500

    Ex36: Added initialization and the first field function.
    Created start(), dead(), field(), river() and cave() functions. I'm trying to chaing
    these together in a way that will make sense. I'm still not sure how to truly
    sanitize input, except that I can shift it to lowercase. ¯\_(ツ)_/¯

--- ex36.py
+++ ex36.py
@@ -1,24 +1,72 @@
+from sys import exit
+import time
+
+
 # begin in a field
 
-# see a river and an entrance to a cave
+def start():
+    print("You awake in a field.")
+    print("The birds are chirping and there is a faint sound of running water.")
+    print("Your eyes open as the sun transits across your face, and you stand up.\n")
+    time.sleep(2)
+
+    field()
+
+# dead function
+def dead(why):
+    print(why, "All done. Bye bye.")
+    exit(0)
 
 # field function
+def field():
+    # see a river and an entrance to a cave
+    print("You are standing in a field.")
+    print("To the right is a river, to the left is a cave.")
+    print("Which way shall you go?")
+    
+    choice = input("> ").lower()
 
     # You can enter the cave
-        # cave function
-
+    if "l" in choice:
+        cave()
     # You can visit the river
-        # You are swept away
+    elif "r" in choice:
+        river()
+    else:
+        print("""You feel indecisive.
+        A pain grips your left arm as your face contorts in a grimace.
+        The feeling that you've just never amounted to much in life enters your mind.
+        """)
+        time.sleep(3)
+        print("""
+        A strong smell of toast fills your nostrils and as you pass out, you think
+        of your wasted youth and the lost feelings of love and graditude that the
+        years of traveling have erased from your life.
+        """)
+        time.sleep(2)
+        print("""
+        The last time you thought of your sister and how long ago you last spoke,
+        you only felt shame.
+        """)
+        time.sleep(2)
+        dead("You have had a heart attack.")
+
+
 
 # river function
-    # text about how nice the water is
-    # you hear a beautiful song of a siren
-    # the water is so inviting
-    # you step further into the water
-    # you begin to drift to sleep
-    # you are swept away and drown, blissfully
+def river():
+    print("""You walk towards the sound of the river.
+As you approach the water, you can faintly hear a sweet melody.
+You step into the glistening water.
+The deeper you go the stronger the song becomes
+The water rises; as it passes your chin you feel warm and safe
+The water enters your mouth as you drift into a dream, and you drown.""")
+
+    dead("You have died.")
 
 # cave function
+def cave():
+    pass
     # you see an armory
         # armory function
 
@@ -61,3 +109,4 @@
 # passage function
     # ???
     # profit!
+start()
